84038dc6ae71a136b185d972f1bdbfa844e0d8e7
HanouneCamara <Alicmr13@gmail.com>
2023-04-26 18:35:13 +0000

    mod form

--- lib/screens/Profile_Page.dart
+++ lib/screens/Profile_Page.dart
@@ -12,102 +12,95 @@ class _Profile_PageState extends State<Profile_Page> {
   final _formKey = GlobalKey<FormState>();
 
   String _numeroTelephone = '';
-  String _nom = '';
-  String _prenom = '';
-
+  String _password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [ const
-                    Text( 
-                      '+222 ',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    const SizedBox(width: 6), // Ajoute un petit espace entre le texte et le champ de numéro
-                    Expanded( 
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: 'Numéro de téléphone',
-                        ),
-                    keyboardType: TextInputType.phone,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Veuillez entrer un numéro de téléphone';
-                      } else if (value.length != 8) {
-                        return 'Le numéro de téléphone doit contenir 8 caractères';
-                      } else {
-                        return null;
-                      }
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        _numeroTelephone = value;
-                      });
-                    },
-                  ),)
-                ],
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Nom',
+      body: Center(
+       child: SingleChildScrollView(
+          child: Container(
+            width: 300, // Définir la largeur souhaitée pour votre formulaire
+            padding: EdgeInsets.all(16),
+            child:
+              Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 32), // Ajoute un petit espace entre le haut de l'écran et le texte "Se connecter"
+              Text(
+                'Se connecter',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer un nom';
-                  } else {
-                    return null;
-                  }
-                },
-                onChanged: (value) {
-                  setState(() {
-                    _nom = value;
-                  });
-                },
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Prénom',
+              SizedBox(height: 32), // Ajoute un petit espace entre le texte "Se connecter" et le formulaire
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [ const
+                      Text( 
+                        '+222 ',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(width: 6), // Ajoute un petit espace entre le texte et le champ de numéro
+                      Expanded( 
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Numéro de téléphone',
+                          ),
+                      keyboardType: TextInputType.phone,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Veuillez entrer un numéro de téléphone';
+                        } else if (value.length != 8) {
+                          return 'Le numéro de téléphone doit contenir 8 caractères';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          _numeroTelephone = value;
+                        });
+                      },
+                    ),)
+                  ],
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Veuillez entrer un prénom';
-                  } else {
-                    return null;
-                  }
-                },
-                onChanged: (value) {
-                  setState(() {
-                    _prenom = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Envoi du formulaire
-                    print('Numéro de téléphone : $_numeroTelephone');
-                    print('Nom : $_nom');
-                    print('Prénom : $_prenom');
-                  }
-                },
-                child: const Text('Envoyer'),
-              ),
-            ],
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Mot de passe',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Veuillez entrer votre mot de passe';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      _password = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Envoi du formulaire
+                      print('Numéro de téléphone : $_numeroTelephone');
+                      print('Mot de passe : $_password');
+                    }
+                  },
+                  child: const Text('Connexion'),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+      ),)
     );
   }
 }
