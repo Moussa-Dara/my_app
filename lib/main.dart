cd7f7cfeb842e09c67a2fa686492363fc7547499
[Moussa] <[Moussa4870@gmail.com]>
2023-04-29 14:26:25 +0000

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/Home_Page.dart';
 import 'screens/Profile_Page.dart';
 import 'screens/Search_Page.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -19,7 +24,6 @@ class _MyAppState extends State<MyApp> {
   //sauvegarder le nmr actu de la page
 
   int _currentIndex = 0;
-  
 
   //mettre a jour le nmr de page
   setCurrentIndex(int index) {
@@ -31,21 +35,21 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blue,
-            elevation: 0,
-            centerTitle: true,
-            // ignore: prefer_const_constructors
-            title: Text(
-              'C A R L O C',
-              style: GoogleFonts.satisfy(textStyle: TextStyle(fontSize: 35), color: Colors.black) ,
-            ),
-            
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          elevation: 0,
+          centerTitle: true,
+          // ignore: prefer_const_constructors
+          title: Text(
+            'C A R L O C',
+            style: GoogleFonts.satisfy(
+                textStyle: TextStyle(fontSize: 35), color: Colors.black),
           ),
+        ),
         //Naviguer entre les pages
-        body: const [ Home_Page(), Search_Page(), Profile_Page()][_currentIndex],
+        body: const [Home_Page(), Search_Page(), Profile_Page()][_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
             currentIndex: _currentIndex,
             onTap: (index) => setCurrentIndex(index),
@@ -60,11 +64,17 @@ class _MyAppState extends State<MyApp> {
             selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
-                icon: Icon(Icons.home), label: 'Accueil',),
+                icon: Icon(Icons.home),
+                label: 'Accueil',
+              ),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.search), label: 'Chercher',),
+                icon: Icon(Icons.search),
+                label: 'Chercher',
+              ),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.person), label: 'Profil',),
+                icon: Icon(Icons.person),
+                label: 'Profil',
+              ),
             ]),
       ),
     );
